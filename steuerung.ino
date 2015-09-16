c557b7bda9dba7b9ed5032bb8868c4a3a12dd11d
electronicfreak <github@electronicfreak.de>
2015-09-16 22:11:15 +0200

    v7

--- steuerung.ino
+++ steuerung.ino
@@ -1,4 +1,4 @@
-// Briefkastensreuerung Stand: 23.05.2015
+// Briefkastensreuerung Stand: 23.08.2015
 // Written by Matthias Teeken
 
 // Definieren der Hardwarepins zu Variablen
@@ -21,11 +21,19 @@ const int Schalter_auf   = PD_2;
 const int Schalter_zu    = PD_3;
 
 //Lichtschranke
-const int LS1         = PE_3; //Lichtschranke vorne 
-const int LS2         = PA_6; //Lichtschranke hinten
+const int LS1         = PE_3;  //Lichtschranke vorne
+const int LS11        = PD_0;  //Lichtschranke vorne Dunkel
+const int LS2         = PA_6;  //Lichtschranke hinten
+const int LS21        = PD_1;  //Lichtschranke hinten Dunkel
 
 // Pin für das Abschalten der Spannung
 const int Aus_sig    = PF_2;
+
+// LED StatusBelegung
+const int LED_green  = PF_1;
+//const int LED_blue   = PF_2;
+const int LED_red    = PF_3;
+
 //___________________________________________
 
 //Definieren von Variablen und deren Anfangswerte 
@@ -36,7 +44,7 @@ int W_Sense_mean      = 0;
 
 int K_Sense_value     = 0;
 int K_Sense_value_map = 0;
-int K_Sense_max       = 5000; //Einstellen des Wertes zum wieder öffnen der Klappe
+int K_Sense_max       = 3900; //Einstellen des Wertes zum wieder öffnen der Klappe
 int K_Sense_mean      = 0;
 
 unsigned long t_start      = 0;
@@ -59,11 +67,21 @@ void setup()
   pinMode(Klappe_Enable,  OUTPUT);
     
   pinMode(LS1, INPUT);
+  pinMode(LS11, INPUT);
   pinMode(LS2, INPUT);
+  pinMode(LS21, INPUT);
 
 // Definieren der Hardwarepins mit Pullup-Widerstand
   pinMode(Schalter_auf,    INPUT_PULLUP);
   pinMode(Schalter_zu,     INPUT_PULLUP);
+
+// Definiieren des Aussignalschalterpins  
+  pinMode(Aus_sig, OUTPUT); 
+ 
+// Definieren der LED Status LED's
+  pinMode(LED_green, OUTPUT);
+  pinMode(LED_red,   OUTPUT);
+//  pinMode(LED_blue,  OUTPUT);
     
 // Definieren der Anfangszustände der verwendeten HArdwarepins, alle auf LOW 
   digitalWrite(Walze_Vor,    LOW);
@@ -71,7 +89,13 @@ void setup()
   digitalWrite(Walze_Enable, LOW);
   digitalWrite(Klappe_Vor,    LOW);
   digitalWrite(Klappe_Back,   LOW);
-  digitalWrite(Klappe_Enable, LOW);  
+  digitalWrite(Klappe_Enable, LOW);
+  digitalWrite(Aus_sig,       LOW);
+  digitalWrite(LED_green,       LOW);
+  digitalWrite(LED_red,       LOW);
+//  digitalWrite(Aus_sig,       LOW);
+  
+
 }
 
 void loop() 
@@ -85,14 +109,22 @@ void loop()
    // Signal geben um aufzufahren.
    digitalWrite(Klappe_Back, LOW);
    digitalWrite(Klappe_Vor, HIGH);
+
+//Beim Auffahren der Klappe leuchtet StatusLED GRÜN
+   digitalWrite(LED_green, HIGH);
+   digitalWrite(LED_red, LOW);
    
-   while(digitalRead(Schalter_auf) == HIGH){     
-     digitalWrite(Klappe_Vor, HIGH);
-     digitalWrite(Klappe_Enable, HIGH);    
-   }
-   // Abschalten der Signale für die Klappe
-   digitalWrite(Klappe_Vor, LOW);
-   digitalWrite(Klappe_Enable, LOW);   
+     while(digitalRead(Schalter_auf) == LOW){     
+       digitalWrite(Klappe_Vor, HIGH);
+       digitalWrite(Klappe_Enable, HIGH);            
+ }
+     // Abschalten der Signale für die Klappe
+     digitalWrite(Klappe_Vor, LOW);
+     digitalWrite(Klappe_Enable, LOW);
+
+   digitalWrite(LED_green, LOW);
+   digitalWrite(LED_red, LOW);
+   
 //________________________________________________
 
 
@@ -100,8 +132,13 @@ void loop()
 //________________________________________________
 t_start   = millis();
 t_aktuell = millis();
- while(t_aktuell < (t_start+t_soll_Walze) || digitalRead(LS1) == LOW || digitalRead(LS2) == LOW ) 
+ while(t_aktuell < (t_start+t_soll_Walze) || digitalRead(LS1) == LOW || digitalRead(LS2) == LOW || digitalRead(LS11) == LOW || digitalRead(LS21) == LOW )
  {  
+ //STaus LED leuchtet ROT wenn Walze einfährt
+   digitalWrite(LED_green, LOW);
+   digitalWrite(LED_red, HIGH);
+ 
+   
   digitalWrite(Walze_Vor, HIGH);
   digitalWrite(Walze_Enable, HIGH);
   t_aktuell = millis();
@@ -128,12 +165,20 @@ t_aktuell = millis();
   // Stoppen der Walze
   digitalWrite(Walze_Vor, LOW);
   digitalWrite(Walze_Enable, LOW);
+  
+   digitalWrite(LED_green, LOW);
+   digitalWrite(LED_red, LOW);
+  
 //________________________________________________
 
 //Klappe schließen
 //________________________________________________
   delay(1000);
   // Signal für Klappe Schießen
+  
+   digitalWrite(LED_green, HIGH);
+   digitalWrite(LED_red, HIGH);
+  
   digitalWrite(Klappe_Enable, HIGH);
   digitalWrite(Klappe_Back, HIGH);
   delay (1000);
@@ -144,17 +189,21 @@ t_aktuell = millis();
      digitalWrite(Klappe_Enable, HIGH);
       
       // Mittelwert von 10 Messwerten aufnehmen
-      for (int x = 0; x <20 ;x++) {
+      for (int x = 0; x <10 ;x++) {
       K_Sense_value = analogRead(Klappe_Sens);
       K_Sense_mean = K_Sense_mean + K_Sense_value;
       delay(10);
       }
-      K_Sense_mean = K_Sense_mean /20;       
+      K_Sense_mean = K_Sense_mean /10;       
       
      // Vergleich ob Mittelwert des Signals über dem Max Wert liegt.
      // Wenn das der Fall ist oder Lichtschranke LS1 was sieht, wieder auffahren.
-     if (digitalRead(LS1) == LOW || K_Sense_mean >= K_Sense_max){
-       digitalWrite(Klappe_Back,LOW);       
+     if (digitalRead(LS1) == LOW || K_Sense_mean >= K_Sense_max){  // || digitalRead(LS11) == LOW
+       digitalWrite(Klappe_Back,LOW);
+       //STausLED wieder löschen
+       digitalWrite(LED_green, HIGH);
+       digitalWrite(LED_red, HIGH);
+       
        goto open_again;
        }       
    }
